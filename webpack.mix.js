03783873f8b64b4631b47e61f07e262d65a8f69c
TobyCulpin <Toby.Culpin@hotmail.co.uk>
2020-05-22 20:26:19 +0100

    reordered the navigation bar

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,4 +13,5 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
-    .sass('resources/sass/navigation-bar.scss', 'public/css');
+    .sass('resources/sass/top-bar.scss', 'public/css')
+    .sass('resources/sass/side-bar.scss', 'public/css');
