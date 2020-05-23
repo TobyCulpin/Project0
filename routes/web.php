a7b2dbd69e4c3e8018a4c0866a2d198c2c5ad535
Jamie Frost <Jamcris11@hotmail.co.uk>
2020-05-23 16:23:41 +0100

    New: Added login route to LoginController

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'HomeController@show');
 Route::get('/admin', 'AdminController@show')->middleware('auth');
+Route::get('/login', 'LoginController@show');
