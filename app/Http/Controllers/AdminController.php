16a239160ba8d57d7f35703a6b4cab516f0920ef
Jamie Frost <Jamcris11@hotmail.co.uk>
2020-05-23 21:47:25 +0100

    New: added user_panel method that returns all users from the users table

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class AdminController extends Controller
 {
@@ -13,6 +14,15 @@ class AdminController extends Controller
 
 	public function index()
 	{
-		return view('admin');
+		return view('admin-dashboard');
+	}
+
+	public function user_panel()
+	{
+		$users = User::select('users.id', 'user_types.name as type', 'users.name', 'users.created_at', 'users.updated_at')
+			->join('user_types', 'users.id', '=', 'user_types.id')
+			->get();
+
+		return view('admin.user-panel', ['users' => $users]);
 	}
 }
