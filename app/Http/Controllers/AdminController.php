5c7dd16bddade53f64761f751bd8f2353c173637
Jamie Frost <Jamcris11@hotmail.co.uk>
2020-05-24 16:48:28 +0100

    New: method that deals with edit page

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -26,4 +26,14 @@ class AdminController extends Controller
 
 		return view('admin.user-panel', ['users' => $users]);
 	}
+
+
+	public function edit_user($id)
+	{
+		$user = User::select('users.*')
+			->where('users.id', '=', $id)
+			->get();
+
+		return view('admin.edit-user', ['user' => $user]);
+	}
 }
