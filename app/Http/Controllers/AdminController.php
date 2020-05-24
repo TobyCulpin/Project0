ae69bde29749b6f9ed5ef3f9e3b5afe275ecb384
Jamie Frost <Jamcris11@hotmail.co.uk>
2020-05-24 17:17:57 +0100

    Fix: incorrect reference to column query

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -21,7 +21,7 @@ class AdminController extends Controller
 	public function user_panel()
 	{
 		$users = User::select('users.id', 'user_types.name as type', 'users.name', 'users.created_at', 'users.updated_at')
-			->join('user_types', 'users.id', '=', 'user_types.id')
+			->join('user_types', 'users.user_type_id', '=', 'user_types.id')
 			->get();
 
 		return view('admin.users', ['users' => $users]);
