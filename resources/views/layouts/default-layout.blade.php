073ee1527e09e01c6594d5a1da6cfafd984e376e
TobyCulpin <Toby.Culpin@hotmail.co.uk>
2020-05-26 21:31:32 +0100

    sidebar is tasty x

--- resources/views/layouts/default-layout.blade.php
+++ resources/views/layouts/default-layout.blade.php
@@ -1,8 +1,8 @@
 <html>
 
 <head>
-	<title>ProjectZero - @yield('title')</title>
-	<link rel="stylesheet" type="text/css" href="{{ asset('/css/top-bar.css') }}">
+        <title>ProjectZero - @yield('title')</title>
+        <link rel="stylesheet" type="text/css" href="{{ asset('/css/top-bar.css') }}">
         <link rel="stylesheet" type="text/css" href="{{ asset('/css/side-bar.css') }}">
 
         <script defer src="side-bar.js"></script>
@@ -15,17 +15,25 @@
         <div class="content">
                 @yield('content')
         </div>
-        
+
 
 </body>
 
 <footer>
         <div>
-                Icons made by 
+                Icons made by
                 <a href="https://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect">Pixel perfect</a>
-                from 
+                from
                 <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
         </div>
+
+        <div>
+                Icons made by <a href="https://smashicons.com/" title="Smashicons">Smashicons</a> from <a
+                        href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+        </div>
+        <div>
+                Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a
+                        href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a></div>
 </footer>
 
 </html>
