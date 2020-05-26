94da128ac0f54eb03342a64e7f4dc47eeb10c839
TobyCulpin <Toby.Culpin@hotmail.co.uk>
2020-05-26 15:55:18 +0100

    added _global-variables.scss file

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,4 +14,5 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .sass('resources/sass/top-bar.scss', 'public/css')
-    .sass('resources/sass/side-bar.scss', 'public/css');
+    .sass('resources/sass/side-bar.scss', 'public/css')
+    .sass('resources/sass/_global-variables.scss', 'public/css');
