1a5718096f0da83fee4898a7a6755384a2c1a3fd
Jamie Frost <Jamcris11@hotmail.co.uk>
2020-05-28 11:54:30 +0100

    New: Login button now shows users name if logged in

--- resources/views/addons/top-bar.blade.php
+++ resources/views/addons/top-bar.blade.php
@@ -29,7 +29,7 @@
                         </a>
                 </li>
                 <li class="item">
-                        <p>Login:</p>
+                        <p>{{ Auth::user()->name ?? 'Login' }}</p>
                 </li>
 
         </ul>
