1637d9b08de2a8f61740782566dc9690f4017b9b
jamcris11 <jamcris11@hotmail.co.uk>
2020-05-31 23:03:14 +0100

    Alt: now only grabs 12 results and reverses the array

--- app/Http/Controllers/VideosController.php
+++ app/Http/Controllers/VideosController.php
@@ -9,9 +9,9 @@ class VideosController extends Controller
 {
         public function show()
 	{
-		$req = Youtube::getPlaylistItemsByPlaylistId('PLNVA8V4eec_sIuoMhv5tPn_0N425hgwkJ');
+		$req = Youtube::getPlaylistItemsByPlaylistId('PLNVA8V4eec_sIuoMhv5tPn_0N425hgwkJ', '', 12);
 
-		$videos = $req['results'];
+		$videos = array_reverse($req['results']);
 		return view('videos', ['videos' => $videos]);
 	}
 }
