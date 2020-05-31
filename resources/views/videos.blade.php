e00c20f0434407960dbea921cff7041c2e02bc87
Jamie Frost <Jamcris11@hotmail.co.uk>
2020-05-31 16:13:11 +0100

    Test: added test embed video

--- resources/views/videos.blade.php
+++ resources/views/videos.blade.php
@@ -5,4 +5,5 @@
 
 @section('content')
 	<h1>Videos</h1>
+	<iframe width="560" height="315" src="https://www.youtube.com/embed/e-7SaDN5jYM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 @stop
