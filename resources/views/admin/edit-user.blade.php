44d423386f9e131ec6f31c3ebca8e9ef395691c9
jamcris11 <jamcris11@hotmail.co.uk>
2020-05-31 14:30:32 +0100

    New/alt: edit page now shows update errors and email and name are now disabled

--- resources/views/admin/edit-user.blade.php
+++ resources/views/admin/edit-user.blade.php
@@ -3,19 +3,32 @@
 @section('title', 'Create User')
 
 @section('content')
-	
-	<form action="{{url('/users/update')}}" method="post">
+@if ($errors->any())
+    	<div class="alert alert-danger">
+        	<ul>
+            	@foreach ($errors->all() as $error)
+                	<li>{{ $error }}</li>
+            	@endforeach
+        	</ul>
+    	</div>
+@endif
+
+	<form action="{{url('/users/update')}}" autocomplete="off" method="post">
 		@csrf
 		@foreach ($user as $u)
 		<input id="id" name="id" value="{{ $u->id }}" hidden>
 		<div class="container columns" style="flex-grow: 1">
 			<div class="container rows">
 				<b>Username</b>
-				<input id="name" type="text" name="name" value="{{ $u->name }}" required autocomplete="name" autofocus>
+				<input id="name" type="text" name="name" value="{{ $u->name }}" disabled required>
+			</div>
+			<div class="container rows">
+				<b>Email</b>
+				<input id="email" type="text" name="email" value="{{ $u->email }}" disabled required>
 			</div>
 			<div class="container rows">
 				<b>User Type</b>
-				<select name="user_type_id" id="usertypes">
+				<select name="user_type_id" id="user_types">
 					@foreach ($user_types as $user_type)
 					@if ($u->user_type_id === $user_type->id)
 					<option value="{{ $user_type->id }}" selected>{{ $user_type->name }}</option>
@@ -26,24 +39,20 @@
 				</select>
 			</div>	
 			<div class="container rows">
-				<b>Email</b>
-				<input id="email" type="text" name="email" value="{{ $u->email }}" required autocomplete="email" autofocus>
-			</div>
-			<div class="container rows">
 				<b>Password</b>
-				<input id="password" type="password" name="password" autofocus>
+				<input id="password" type="password" name="password" autocomplete="new-password">
 			</div>
 			<div class="container rows">
 				<b>Confirm password</b>
-				<input id="confirm_password" type="password" autofocus>
+				<input id="confirm_password" name="password_confirmation" type="password">
 			</div>
 			<div class="container rows">
 				<b>Created At</b>
-				<input id="created_at" type="text" name="created_at" value="{{ $u->created_at }}" required autocomplete="created_at" autofocus disabled="disabled">
+				<input id="created_at" type="text" name="created_at" value="{{ $u->created_at }}" required autocomplete="created_at" autofocus disabled>
 			</div>
 			<div class="container rows">
 				<b>Updated At</b>
-				<input id="updated_at" type="text" name="updated_at" value="{{ $u->updated_at }}" required autocomplete="updated_at" autofocus disabled="disabled">
+				<input id="updated_at" type="text" name="updated_at" value="{{ $u->updated_at }}" required autocomplete="updated_at" autofocus disabled>
 			</div>
 			<div class="container rows">
 				<input type="submit" value="Update">
@@ -52,4 +61,11 @@
 		@endforeach
 	</form>
 
+@if (session('success'))
+    <div class="col-sm-12">
+        <div class="alert  alert-success alert-dismissible fade show" role="alert">
+            {{ session('success') }}
+        </div>
+    </div>
+@endif
 @stop
