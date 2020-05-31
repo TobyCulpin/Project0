2ea856ff632c422677c61f06925e122f279419d9
TobyCulpin <Toby.Culpin@hotmail.co.uk>
2020-05-31 16:19:43 +0100

    linked blade.php files to css files

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,6 +3,10 @@
 
 @section('title', 'About')
 
+@section('content-style')
+        <link rel="stylesheet" type="text/css" href="{{ asset('/css/about.css') }}">
+@stop
+
 @section('content')
 	<h1>About</h1>
 @stop
--- resources/views/members.blade.php
+++ resources/views/members.blade.php
@@ -3,6 +3,10 @@
 
 @section('title', 'Members')
 
+@section('content-style')
+        <link rel="stylesheet" type="text/css" href="{{ asset('/css/members.css') }}">
+@stop
+
 @section('content')
 	<h1>Members</h1>
 @stop
